9f14a7c44b52e1c0ee0d66c7e52a6213da6cc13c
Chris <28993122+csinjin@users.noreply.github.com>
2024-10-09 02:26:02 -0700

    Add 16x16 div grid generated in JavaScript
    Each square is 50px x 50px

---
+++ script.js
@@ -0,0 +1,15 @@
+const gridContainer = document.querySelector('#gridContainer');
+
+for (let row = 1; row <= 16; row++){
+    let rowDiv = document.createElement('div');
+    rowDiv.setAttribute('id', ('row' + row));
+    for (let column = 1; column <= 16; column++){
+        let columnDiv = document.createElement('div');
+        columnDiv.setAttribute('id', ('row' + row + 'column' + column));
+        columnDiv.style.width = '50px';
+        columnDiv.style.height = '50px';
+        rowDiv.appendChild(columnDiv);
+    }
+    rowDiv.style = 'display: flex; background: white; width: 800px';
+    gridContainer.appendChild(rowDiv);
+}
